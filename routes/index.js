4685bbf504b1f86b06e554fda22859a421db2b1a
k34425707 <b102145@gmail.com>
2021-12-25 02:19:35 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 const req = require('express/lib/request');
 var router = express.Router();
 var userfunction=require('./userfunction');
-//---------------------------------------------------------------------------------
+//---------------------------userfunction----------------------------------------------------
 router.post('/reg', userfunction.register);
 router.post('/check_login',userfunction.check_login);
-
+router.post('/logout',userfunction.logout);
+//
 module.exports = router;
