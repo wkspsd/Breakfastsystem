ec571add17a946b1874e45d7e24d73bc1a4f0b1a
k34425707 <b102145@gmail.com>
2021-12-24 22:03:36 +0800

    register finish

--- Breakfastsystem/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-const req = require('express/lib/request');
-var router = express.Router();
-var userfunction=require('./userfunction');
-
-router.POST('/user_register', userfunction.register);
-
-module.exports = router;
