c6d4495341b84084b17134080490240cc26d0d79
k34425707 <b102145@gmail.com>
2021-12-25 16:52:53 +0800

    shopkeeper menu CD

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,13 @@ var express = require('express');
 const req = require('express/lib/request');
 var router = express.Router();
 const userfunction=require('./userfunction');
-const mealfunction=require('./mealfunction');
+const menufunction=require('./menufunction');
 //---------------------------userfunction----------------------------------------------------
 router.post('/reg', userfunction.register);
 router.post('/check_login',userfunction.check_login);
 router.post('/logout',userfunction.logout);
-//---------------------------mealfunction----------------------------------------------------
-router.post('/addmeal',mealfunction.addmeal);
-router.get('/showAllmeals',mealfunction.showAllmeals);
+//---------------------------menufunction----------------------------------------------------
+router.post('/editmenuplus.html',menufunction.editmenuplus);
+router.get('/get_menu',menufunction.get_menu);
+router.post('/delete_single',menufunction.delete_single);
 module.exports = router;
