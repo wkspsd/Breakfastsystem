cfe1ec1e86790bc37c6dd75d2b3cf31026331d31
k34425707 <b102145@gmail.com>
2021-12-26 14:09:41 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ const req = require('express/lib/request');
 var router = express.Router();
 const userfunction=require('./userfunction');
 const menufunction=require('./menufunction');
+
 //---------------------------userfunction----------------------------------------------------
-router.post('/reg', userfunction.register);
-router.post('/check_login',userfunction.check_login);
-router.post('/logout',userfunction.logout);
+router.post('/reg', userfunction.register);//註冊
+router.post('/check_login',userfunction.check_login);//登入檢查
+router.post('/logout',userfunction.logout);//登出
+
 //---------------------------menufunction----------------------------------------------------
-router.post('/editmenuplus.html',menufunction.editmenuplus);
-router.get('/get_menu',menufunction.get_menu);
-router.post('/delete_single',menufunction.delete_single);
+router.post('/editmenuplus.html',menufunction.editmenuplus);//店主增加菜色
+router.get('/get_menu',menufunction.get_menu);//顧客登入時獲取所有菜色
+router.post('/delete_single',menufunction.delete_single);//
 module.exports = router;
