25477c68632f934369af8db62153d7ad1bf59793
wally929 <95478358+wally929@users.noreply.github.com>
2022-01-19 15:42:11 +0800

    Add files via upload

---
+++ menufunction.js
@@ -0,0 +1,108 @@
+const singlemodel=require('./model/single');
+const setmodel=require('./model/set');
+const { set } = require('mongoose');
+
+//新增餐點
+exports.editmenuplus=function(req,res){
+    console.log(req.body.foodname);
+    singlemodel.create({
+        food_name:req.body.foodname,
+        price:req.body.price,
+        description:req.body.description
+    },function(err){
+        if(err)
+        {
+            console.log("add fail");
+        }
+        else
+        {
+            console.log("add success");
+        }
+    });
+    res.redirect('editmenu.html')
+}
+
+//店家刪除餐點
+exports.delete_single=function(req,res){
+
+    singlemodel.findByIdAndDelete(req.body.deleteid,function(err){
+        if(err)
+        {
+            console.log("delete fail");
+        }
+        else
+        {
+            console.log("delete success");
+        }
+    })
+    res.redirect('editmenu.html');
+}
+
+//獲取全部餐點資料
+exports.get_menu=function(req,res){
+    singlemodel.find({},function(err,result){
+        //console.log(result);
+        console.log(req.session.user);
+        res.send(result);
+    });
+}
+
+//店家修改餐點
+exports.modify_single=function(req,res){
+    singlemodel.findByIdAndUpdate(req.body.foodid,{
+        food_name:req.body.foodname,
+        price:req.body.price,
+        description:req.body.description
+    },function(err){
+        if(err){
+            console.log("modify fail");
+        }
+        else{
+            console.log("modify success");
+        }
+    });
+    res.redirect('editmenu.html');
+}
+//店家新增套餐
+exports.new_set=function(req,res){
+    console.log(req.body);
+    if(Array.isArray(req.body.foodarray))
+    {
+        setmodel.create({
+            set_name:req.body.name,
+            price:req.body.price,
+            description:req.body.description,
+            food_id:req.body.foodarray
+        },function(err){
+            if(err)throw err;
+    })
+}
+    res.redirect('/manage.html');
+}
+
+//刪除套餐
+exports.delete_set=function(req,res){
+    set.findByIdAndDelete(req.body.deleteid,function(err){
+        if(err) throw err;
+    })
+    res.redirect('./manage.html')
+}
+
+//獲取所有套餐資訊
+exports.get_set=function(req,res){
+    setmodel.find({},function(err,result){
+        if(err) throw err;
+        res.send(result);
+    })
+}
+
+//修改套餐資訊
+exports.modify_set=function(req,res){
+    setmodel.findByIdAndUpdate(req.body.setid,{
+        set_name:req.body.setname,
+        price:req.body.price,
+        description:req.body.description,
+        food_id:req.body.foodarray
+    });
+    res.redirect('./manage.html');
+}
---
+++ userfunction.js
@@ -0,0 +1,72 @@
+const req = require('express/lib/request');
+var mongoose=require('mongoose');
+const usermodel=require('./model/user');
+const session=require('express-session');
+const bcrypt=require('bcrypt');
+//註冊
+exports.register=function(req,res){
+    usermodel.findOne({account:{$eq:req.body.account}},function(err,result)
+    {
+        console.log("result is "+result);
+        if(result)//帳號已存在
+        {
+            console.log("user_accout exist");
+            //res.send({success:false});
+            res.redirect('./reg.html');
+        }
+        else//帳號不存在
+        {
+            usermodel.create({
+                user_name:req.body.username,
+                age:req.body.age,
+                gender:req.body.gender,
+                account:req.body.account,
+                password:bcrypt.hashSync(req.body.password,5)
+            },function(err){
+                console.log(err);
+            })
+            console.log("user: "+req.body.username+" create!");
+            //res.send({success:true});
+            res.redirect('index.html');
+        }
+    })
+}
+
+//登入檢查
+exports.check_login=function(req,res){
+    if(req.body.account=="shopkeeper"&& req.body.password=="shopkeeper")
+    {
+        req.session.user="shopkeeper";
+        res.redirect('manage.html');
+    }
+    else
+    {
+        usermodel.findOne({account:{$eq:req.body.account}},function(err,result)
+        {
+            if(err) throw err;
+            if(result && bcrypt.compareSync(req.body.password,result.password))//如果找得到符合比對的資料
+            {
+                console.log("login success");
+                req.session.user=result.user_name;
+                res.redirect('/menu.html');
+            }
+            else
+            {
+                console.log("login fail");
+                res.redirect('/index.html');
+            }
+        });
+    }
+}
+
+//登出
+exports.logout=function(req,res){
+    req.session.destroy();
+    res.redirect('index.html');
+}
+
+//拿到user_name
+exports.get_username=function(req,res){
+    console.log(req.session.user);
+    res.send({username:req.session.user});
+}
